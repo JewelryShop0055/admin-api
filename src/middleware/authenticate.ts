ddd8b55c0583d4771f9b29d74f9987690e67188d
leXia Lim <limlexia@gmail.com>
2021-09-15 18:04:26 +0900

    Public/Secret 분배 미들웨어 추가

---
+++ src/middleware/authenticate.ts
@@ -0,0 +1,14 @@
+import { NextFunction, Request, Response } from "express";
+import oAuth2Server from "../oauth/server";
+
+export default function authenticate(isPublic = false) {
+  return (req: Request, res: Response, next: NextFunction) => {
+    const hasToken = req.headers.authorization;
+
+    if (!hasToken && isPublic) {
+      return next();
+    } else {
+      return oAuth2Server.authenticate()(req, res, next);
+    }
+  };
+}
