e11f0898fcc2694768ff9f6c49005340676a1538
leXia Lim <limlexia@gmail.com>
2021-09-17 17:59:12 +0900

    ItemUnitType: 캐럿, 돈 단위 추가

--- src/model/itemType.ts
+++ src/model/itemType.ts
@@ -11,14 +11,34 @@ export const ItemTypeEnum = DataTypes.ENUM({
   values: ["product", "parts"],
 });
 
+/**
+ * 아이템 단위
+ */
 export const ItemUnitTypes = {
+  /**
+   * 갯 수
+   */
   ea: "ea",
+  /**
+   * 그람
+   */
   g: "g",
+  /**
+   * 킬로그람
+   */
   kg: "kg",
-};
+  /**
+   * 캐럿
+   */
+  carat: "carat",
+  /**
+   * 돈
+   */
+  mace: "mace",
+} as const;
 
 export type ItemUnitType = typeof ItemUnitTypes[keyof typeof ItemUnitTypes];
 
 export const ItemUnitTypeEnum = DataTypes.ENUM({
-  values: ["ea", "g", "kg"],
+  values: ["ea", "g", "kg", "carat"],
 });
