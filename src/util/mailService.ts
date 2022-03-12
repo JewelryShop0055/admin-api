57a1fd6122c94fe653e90e307df8c8c676831f5b
leXia Lim <limlexia@gmail.com>
2022-03-12 10:09:55 +0900

    mail template 관련 수정, mail config sample 수정

--- src/util/mailService.ts
+++ src/util/mailService.ts
@@ -8,21 +8,18 @@ import nodemailer from "nodemailer";
 export class MailService {
   private transporter = nodemailer.createTransport(config.mail);
 
-  private passwordTemplate = fs.readFileSync(
-    path.join(__dirname, "forgotpassword-admin.html"),
-    {
+  private passwordTemplate = ejs.compile(
+    fs.readFileSync(path.join(__dirname, "forgotpassword-admin.html"), {
       encoding: "utf-8",
-    },
+    }),
   );
 
   async sendChangePassword(user: User, newPassword: string) {
-    const template = ejs.compile(this.passwordTemplate);
-
     await this.transporter.sendMail({
       from: `${config.app.serviceName} console <${config.app.systemEMailAddress}>`, // sender address
       to: user.email, // list of receivers
       subject: `[${config.app.serviceName} SMS] ${user.name}님의 임시 비밀번호입니다.`, // Subject line
-      html: template({
+      html: this.passwordTemplate({
         user,
         newPassword,
         serviceUrl: config.app.serviceUrl,
